9121dc0d1e28863842b9c228a57d1a5b3346de99
John Zheng <jz31337@hotmail.ca>
2018-07-16 23:05:34 -0700

    Vincent and Justin file merged so far

---
+++ mealify/View/UINavigationController.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationController.h
+//  mealify
+//
+//  Created by John Zheng on 2018-07-16.
+//  Copyright © 2018 Meal Mules. All rights reserved.
+//
+
+#ifndef UINavigationController_h
+#define UINavigationController_h
+
+
+#endif /* UINavigationController_h */
